5a08b60a44175c51ba4b31308485aebc773cee87
Yannick Metz <y1metz@hotmail.de>
2020-05-01 12:46:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ultrascale',
+      version='0.0.1',
+      description='Large-Scale Agent-Based Simualtion Framework with Machine-Learning Support',
+      url='https://github.com/ymetz/ultrascale',
+      author='Yannick Metz',
+      author_email='yannick.metz@uni-konstanz.de',
+      license='MIT',
+      packages=['ultrascale'],
+      zip_safe=False)
